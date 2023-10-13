79993ce6fa4b71dc96a9504db3976c37fefa0c16
eric <ericmarcelo455@gmail.com>
2023-10-12 17:58:38 -0700

    Primitive Types
    * primitive divide (reformatted file)

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -2,8 +2,18 @@ from test_framework import generic_test
 
 
 def divide(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    p = 0
+    v = y
+    r = 1
+    while x > v:
+        v <<= 1
+        p += 1
+    r <<= p
+
+    while x < v:
+        v -= y
+        r -= 1
+    return r
 
 
 if __name__ == '__main__':
