7b3e0226c429ceb01ef638064b654e573ae55e59
eric <ericmarcelo455@gmail.com>
2023-10-18 07:55:25 -0700

    Arrays
    * int as array increment

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -7,6 +7,20 @@ def plus_one(A: List[int]) -> List[int]:
     i = len(A) - 1
     b = []
 
+    while i >= 0 and A[i] == 9:
+        b.insert(0, 0)
+        i -= 1
+
+    if i >= 0:
+        b.insert(0, A[i] + 1)
+        i -= 1
+
+    while i >= 0:
+        b.insert(0, A[i])
+        i -= 1
+
+    if A[0] == 9 and b[0] == 0:
+        b.insert(0, 1)
     return b
 
 
