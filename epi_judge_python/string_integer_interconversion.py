cd0b06804969621319e47c18a573da4c683ea1f1
eric <ericmarcelo455@gmail.com>
2023-11-02 16:19:58 -0700

    Strings
    * is string palindromic
    * string interger interconversion

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -3,13 +3,44 @@ from test_framework.test_failure import TestFailure
 
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    neg = False
+    r = []
+    s = ''
+    if x < 0:
+        neg = True
+        x *= -1
+
+    if x == 0:
+        return '0'
+
+    while x > 0:
+        v = x % 10
+        x //= 10
+        r.insert(0, str(v))
+
+    if neg:
+        s += "-"
+    for i in range(len(r)):
+        s += r[i]
+    return s
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    neg = False
+    b = 0
+    if s[0] == '-':
+        neg = True
+    if s[0] == '+' or s[0] == '-':
+         b = 1
+    d = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+    v = 0
+    for i in range(b, len(s)):
+        v *= 10
+        v += d.get(s[i])
+
+    if neg:
+        v *= -1
+    return v
 
 
 def wrapper(x, s):
