aad918173c73f2cf277620d959b990574da7833b
eric <ericmarcelo455@gmail.com>
2023-11-05 07:45:41 -0800

    Linked Lists
    * delete from list
    * insert in list
    * search in list
    * sorted lists merge

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -6,8 +6,36 @@ from test_framework import generic_test
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+
+    if not L1 and not L2:
+        return None
+    elif not L1:
+        return L2
+    elif not L2:
+        return L1
+    elif L1.data <= L2.data:
+        R = L1
+        L1 = L1.next
+    else:
+        R = L2
+        L2 = L2.next
+
+    Head = R
+    while L1 and L2:
+        if L1.data <= L2.data:
+            R.next = L1
+            L1 = L1.next
+        else:
+            R.next = L2
+            L2 = L2.next
+        R = R.next
+
+    if L1:
+        R.next = L1
+    elif L2:
+        R.next = L2
+
+    return Head
 
 
 if __name__ == '__main__':
