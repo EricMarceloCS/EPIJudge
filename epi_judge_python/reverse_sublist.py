60a8109fd283055d0e1a38082991d283bdfb12e3
eric <ericmarcelo455@gmail.com>
2023-11-07 16:52:43 -0800

    Linked Lists
    * Reverse Sublist

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -6,8 +6,38 @@ from test_framework import generic_test
 
 def reverse_sublist(L: ListNode, start: int,
                     finish: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+
+    if not L or start == finish:
+        return L
+    n1 = 1
+    n2 = 1
+    f = L
+    while n2 != finish:
+        f = f.next
+        n2 += 1
+
+    p = None
+    c = L
+    n = c.next
+    rev = False
+    while n1 <= finish:
+        if n1 == start:
+            c.next = f.next
+            rev = True
+            if p:
+                p.next = f
+        elif rev:
+            c.next = p
+        p = c
+        c = n
+        if n:
+            n = n.next
+        n1 += 1
+
+    if start == 1:
+        return p
+    else:
+        return L
 
 
 if __name__ == '__main__':
