8b9a6fb49b19a13dad96d11c8428002cb1fff534
eric <ericmarcelo455@gmail.com>
2023-11-09 09:30:44 -0800

    Stacks And Queues
    * tree level order

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -5,8 +5,37 @@ from test_framework import generic_test
 
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    r = []
+    q1: List = [tree]
+    q2: List = []
+
+    while q1 or q2:
+        l = []
+        while q1:
+            n = q1.pop(0)
+            if n:
+                if n and n.left:
+                    q2.append(n.left)
+                if n and n.right:
+                    q2.append(n.right)
+                l.append(n.data)
+
+        if len(l) != 0:
+            r.append(l)
+        l = []
+
+        while q2:
+            n = q2.pop(0)
+            if n:
+                if n.left:
+                    q1.append(n.left)
+                if n.right:
+                    q1.append(n.right)
+                l.append(n.data)
+        if len(l) != 0:
+            r.append(l)
+
+    return r
 
 
 if __name__ == '__main__':
