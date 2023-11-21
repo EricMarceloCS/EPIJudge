92c6d8169aec2117660a5a2bbe1adc0ccf387350
eric <ericmarcelo455@gmail.com>
2023-11-21 08:07:55 -0800

    Search
    * int square root
    * search first key

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -1,9 +1,29 @@
+import math
+
 from test_framework import generic_test
 
 
 def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    lp = 1
+    rp = k
+
+    while lp < rp:
+        m = lp + ((rp - lp) // 2)
+        rt = m ** 2
+        if rt == k:
+            return m
+        elif rt < k:
+            lp = m + 1
+        elif rt > k:
+            rp = m - 1
+
+    while True:
+        if lp ** 2 > k:
+            lp -= 1
+        else:
+            break
+
+    return lp
 
 
 if __name__ == '__main__':
--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -16,21 +16,18 @@ def search_first_of_k(A: List[int], k: int) -> int:
 
     while lp < rp:
         m = lp + ((rp - lp) // 2)
-        if A[lp] == k:
-            r = lp
-            break
-        elif A[rp] == k:
-            r = rp
-            break
-        elif A[m] == k:
+        if A[m] == k:
             r = m
             break
         elif A[m] > k:
             rp = m - 1
         elif A[m] < k:
             lp = m + 1
-        if lp == rp:
-            return r
+
+    if lp == rp and A[lp] == k:
+        r = lp
+    elif lp == rp and A[lp] != k:
+        return -1
 
     while True:
         if A[r-1] == k:
