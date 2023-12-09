83a011878ac6b77c14cb5ce0f7453dd40fccec38
eric <ericmarcelo455@gmail.com>
2023-12-08 17:02:41 -0800

    Binary Search Trees
    * search first greater value

--- epi_judge_python/search_first_greater_value_in_bst.py
+++ epi_judge_python/search_first_greater_value_in_bst.py
@@ -5,8 +5,22 @@ from test_framework import generic_test
 
 
 def find_first_greater_than_k(tree: BstNode, k: int) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+    nmv = None
+    while tree is not None and tree.data != k:
+        if tree.data > k and (nmv is None or tree.data < nmv.data):
+            nmv = tree
+        if k > tree.data:
+            tree = tree.right
+        elif k < tree.data:
+            tree = tree.left
+
+    if tree is not None and tree.right:
+        tree = tree.right
+        while tree is not None:
+            nmv = tree
+            tree = tree.left
+
+    return nmv
 
 
 def find_first_greater_than_k_wrapper(tree, k):
