47a750db3315361fc92b1eadbef2581255a2b796
eric <ericmarcelo455@gmail.com>
2023-12-18 11:45:26 -0800

    Linked Lists
    * delete kth last from list

--- epi_judge_python/delete_kth_last_from_list.py
+++ epi_judge_python/delete_kth_last_from_list.py
@@ -6,8 +6,23 @@ from test_framework import generic_test
 
 # Assumes L has at least k nodes, deletes the k-th last node in L.
 def remove_kth_last(L: ListNode, k: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    p1 = L
+    p2 = L
+    i = 0
+    while p1 is not None and i < k + 1:
+        p1 = p1.next
+        i += 1
+
+    while p1 is not None:
+        p1 = p1.next
+        p2 = p2.next
+
+    if i == k:
+        L = L.next
+    else:
+        p2.next = p2.next.next
+
+    return L
 
 
 if __name__ == '__main__':
