a6e1495d535a263c6dd574d579359f41a94e8407
eric <ericmarcelo455@gmail.com>
2024-01-18 11:29:05 -0800

    Binary Trees
    * tree from preorder inorder
      (note: uses integers)

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -6,8 +6,37 @@ from test_framework import generic_test
 
 def binary_tree_from_preorder_inorder(preorder: List[int],
                                       inorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+
+    node_to_inorder_idx = {data: i for i, data in enumerate(inorder)}
+
+    def binary_tree_from_preorder_inorder_helper(preorder_start, preorder_end,
+                                                 inorder_start, inorder_end):
+        if preorder_end <= preorder_start or inorder_end <= inorder_start:
+            return None
+
+        root_inorder_idx = node_to_inorder_idx[preorder[preorder_start]]
+        left_subtree_size = root_inorder_idx - inorder_start
+
+        return BinaryTreeNode(
+            preorder[preorder_start],
+            binary_tree_from_preorder_inorder_helper(
+                preorder_start + 1,
+                preorder_start + 1 + left_subtree_size,
+                inorder_start,
+                root_inorder_idx
+            ),
+            binary_tree_from_preorder_inorder_helper(
+                preorder_start + 1 + left_subtree_size,
+                preorder_end,
+                root_inorder_idx + 1,
+                inorder_end
+            )
+        )
+
+    return binary_tree_from_preorder_inorder_helper(preorder_start=0,
+                                                    preorder_end=len(preorder),
+                                                    inorder_start=0,
+                                                    inorder_end=len(inorder))
 
 
 if __name__ == '__main__':
